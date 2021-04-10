5d27dcfcf4bc4b1770e3df1b64c2afad5f30bce3
DanielMarianoCorazza <81099633+DanielMarianoCorazza@users.noreply.github.com>
2021-04-10 00:08:21 -0300

    Update atividade10.py

--- atividade10.py
+++ atividade10.py
@@ -1,3 +1,19 @@
+#Função 2 -------
+def funcao2():
+  num_entradas = int(input("Digite o numero de entradas: "))
+  lista = []
+  i = 0
+  while i < num_entradas:
+    lista.append(input("Adicione na lista por aqui: "))
+    i += 1
+  return lista
+
+
+print (funcao2())
+
+
+
+#Função 4 ---------
 def menu_futebol():
   opcao = input("Insira aqui a sua opção:")
   while opcao != 'z':
